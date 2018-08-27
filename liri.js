783bf3bd4850ea581026c3e77b8d824c11cc4902
Crystal Ngo <crystalngo88@gmail.com>
2018-08-27 15:57:14 -0700

    most things aren't broken

--- liri.js
+++ liri.js
@@ -98,6 +98,35 @@ function doWhatItSays() {
                     console.log("----------------------------------------")
                 })
             });
+        } else if (data[0] === "movie-this") {
+            var movieUrl = "http://www.omdbapi.com/?t=" + inputString + "&y=&plot=short&apikey=trilogy";
+            request(movieUrl, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    var movie = JSON.parse(body);
+                    console.log("Movie Title: " + movie.Title);
+                    console.log("Year: " + JSON.parse(body).Year);
+                    console.log("IMDB rating: " + JSON.parse(body).imdbRating);
+                    console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1]);
+                    console.log("Country: " + JSON.parse(body).Country);
+                    console.log("Language: " + JSON.parse(body).Language);
+                    console.log("Plot: " + JSON.parse(body).Plot);
+                    console.log("Actors: " + JSON.parse(body).Actors);
+                    console.log("----------------------------------------")
+                }
+            });
+        } else if (data[0] === "concert-this") {
+            var concertURL = "https://rest.bandsintown.com/artists/" + inputString + "/events?app_id=codingbootcamp"
+            request(concertURL, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    var bandArray = JSON.parse(body)
+                    bandArray.forEach(function (event) {
+                        console.log("Venue name: ", event.venue.name)
+                        console.log("Venue city: ", event.venue.city)
+                        console.log("Concert date: ", moment(event.datetime).format('l'))
+                        console.log("----------------------------------------")
+                    })
+                }
+            });
         }
     })
 }
